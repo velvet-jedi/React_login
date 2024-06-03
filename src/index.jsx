4f2de71f81885e36eb33874ab6e41768cd620a1b
velvet-jedi <pranjalruhela2811@gmail.com>
2024-06-03 18:21:23 +0530

    refactor: rename main.js and index.js to main.jsx and index.jsx and update ReactDOM rendering method

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 // entry point for a react application to mount into the DOM
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider'; // provides auth context making auth data accessible to child components 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; // tomamange routing in react app
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode> {/** wraps the entire app for checks and warnings */}
     <BrowserRouter> {/*.. routing capabilities using HTML5 history API. Routing must be available to al the child components */}
     <AuthProvider>  {/* provides auth context to child components. It should be layered on top of all the routes*/}
@@ -16,8 +17,21 @@ ReactDOM.render(
       </Routes>
     </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-document.getElementById('root')
+  </React.StrictMode>
 );
+
+
+// ReactDOM.render(
+//   <React.StrictMode> {/** wraps the entire app for checks and warnings */}
+//     <BrowserRouter> {/*.. routing capabilities using HTML5 history API. Routing must be available to al the child components */}
+//     <AuthProvider>  {/* provides auth context to child components. It should be layered on top of all the routes*/}
+//       <Routes>    {/** container for all route components */}
+//       <Route path='/*' element={<App />} />
+//       </Routes>
+//     </AuthProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>,
+// document.getElementById('root')
+// );
 
 
